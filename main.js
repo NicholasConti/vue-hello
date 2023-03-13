5796e3265305f9b18e835c5e2590f11438111eef
NicholasConti <conti94@live.it>
2023-03-13 16:11:40 +0100

    ad img and event w/data e methods

--- main.js
+++ main.js
@@ -5,7 +5,14 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            url: 'https://picsum.photos/200/300'
+        }
+    },
+    methods: {
+        change(){
+            console.log('ho cliccato');
+            this.message = 'Ho cliccato il bottone!'
         }
     }
 }).mount('#app');
